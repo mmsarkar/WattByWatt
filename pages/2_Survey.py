743c9458437689fc44dd6e37a10783b13da6c0f1
mjboliva <madisonjulianna.oliva@gmail.com>
2023-02-26 02:17:58 -0800

    Added some buttons and text to survey (will need to make the if statements next), connected Google Sheets API and connected our database to the data page (successfully did this with gspread) and put it into tables for each county.

--- pages/2_Survey.py
+++ pages/2_Survey.py
@@ -1,4 +1,21 @@
 import streamlit as st
 
-# please write out the questions
-# and then we implment the buttons or whatever else is needed
+# About the page
+st.title('Energy Survey')
+st.write('Answer these questions to get your own personalized tips on how to make the switch to sustainable energy sources!')
+
+st.subheader('Survey:')
+
+#question 1
+st.write('What county do you live in?')
+countyChoice = st.selextbox('*Choose an option...*', ('Los Angeles', 'Orange', 'Riverside'))
+
+#question 2
+st.write('How many people are in your househoold?')
+householdNumber = st.number_input('*Choose a number...*', min_value = 1, max_value = 10, value = int, step = 1)
+
+#question 3
+st.write('Which of the following would you like to implement in the future?')
+selectedActions = st.multiselect('*Select an option/options...*', ['Solar Panels', ''])
+
+#results from buttons:
--- pages/3_Data.py
+++ pages/3_Data.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import gspread
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
 
 st.set_page_config(page_title="Energy and Finance Data", layout= "wide")
 
@@ -17,13 +19,30 @@ countySelect = st.selectbox(
        "Choose a county:", 
        ("Los Angeles", "Orange", "Riverside"),)
 
+gc = gspread.service_account(filename='/Users/mj/Desktop/WATTSSSS/WattByWatt/.venv/lib/python3.11/site-packages/gspread/watt-by-watt-database-abb4a6c70d6c.json')
+sh = gc.open("WattByWatt")
+worksheet = sh.worksheet('AverageEnergyUsage')
+
 #list 1.1
 if countySelect == "Los Angeles":
     st.subheader("Los Angeles County")
+    
+    #opens the energy usage spreadsheet (google, using Google SHeets API)
+    eu_read = get_as_dataframe(worksheet, usecols = [0, 1], nrows = 6)
+    eu_read
+    #s_range = sh.worksheet("AverageEnergyUsage")
+    #for row in s_range:
+        #for col in row:
+            #print(str(col).rjust(15), end="") # CHECK COLUMN WIDTH
+        #print("")
+
 #list 1.2  
 if countySelect == "Orange":
     st.subheader("Orange County")
+    eu_read = get_as_dataframe(worksheet, usecols = [0, 2], nrows = 6)
+    eu_read
 #list 1.3
 if countySelect == "Riverside":
     st.subheader("Riverside County")
-    
+    eu_read = get_as_dataframe(worksheet, usecols = [0, 3], nrows = 6)
+    eu_read
